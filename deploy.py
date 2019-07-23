fb26aa8be30a8c9940efb8be4fac3bf32e2c45bc
aaaa3293823524 <3293823524@qq.com>
2019-07-23 21:32:20 +0800

    Auto Deployment

--- deploy.py
+++ deploy.py
@@ -9,9 +9,10 @@ cmds = []
 cmds.append('git init')
 cmds.append('git add .')
 cmds.append('git commit -m "Auto Deployment"')
-cmds.append('git remote add origin git@github.com:aaaa3293823524/nowcoder.git')
-cmds.append('git push -u origin master')
-
+# cmds.append('git remote add origin git@github.com:aaaa3293823524/nowcoder.git')
+cmds.append('git push -f -q aaaa3293823524@nowcoder.git master:master')
+# cmds.append('git push -u origin master')
+# git push -f -q aaaa3293823524@nowcoder.git master:master
 for cmd in cmds:
     try:
         ret = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT).decode('utf-8').strip()
